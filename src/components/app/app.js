98afe0ce72456e803869f555124f242881b0d34f
AlexK <aleksandr.ksj@mail.ru>
2020-11-09 13:12:13 +0300

    Created initial files set

---
+++ src/components/app/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>
+			<input type='checkbox'></input>
+			<p>first</p>
+			<input type='checkbox'></input>
+			<p>second</p>
+			<input type='checkbox'></input>
+			<p>third</p>
+
+		</div>
+	);
+}
+export default App; 
